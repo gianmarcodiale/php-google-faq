20ea1349887d7117e9568e89eaf61fc0a1b9133e
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-23 16:45:10 +0200

    dynamically add faqs

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $faqs = [
     [
         "question" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
@@ -33,3 +33,36 @@ $faqs = [
     ]
 ]
 ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Google</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+        <?php foreach ($faqs as $faq) : ?>
+            <h2><?= $faq['question'] ?></h2>
+            <?php foreach ($faq['answer'] as $faq_answer) : ?>
+                <p><?= $faq_answer ?></p>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
+
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
